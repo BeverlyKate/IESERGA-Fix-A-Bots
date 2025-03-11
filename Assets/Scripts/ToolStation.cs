2b1f1319b3d1f2f475de29d379268e727d9e8a97
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2025-03-11 13:56:01 +0800

    Fixed switching camera views

--- Assets/Scripts/ToolStation.cs
+++ Assets/Scripts/ToolStation.cs
@@ -2,15 +2,81 @@ using UnityEngine;
 
 public class ToolStation : MonoBehaviour
 {
+    private GameObject[] missingLocation;
+
+    private Transform chosenBolt;
+
+    private Camera cam;
+
+    private bool draggingObj = false;
+
+    private float deltaPosition;
+
+    private Vector3 originalPosition;
+
+    private Vector3 offset;
+
+    Ray GetRay()=> cam.ScreenPointToRay(Input.mousePosition);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        missingLocation = GameObject.FindGameObjectsWithTag("Screw");
+
+        cam = gameObject.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                HandleClick(Input.GetTouch(0).position);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!draggingObj) {
+                HandleClick(Input.mousePosition);
+            }
+
+            if (draggingObj)
+            {
+                Debug.Log(draggingObj);
+                if(Physics.Raycast(GetRay(), out RaycastHit rayHit))
+                {
+                    chosenBolt.position = rayHit.point;
+                }
+            }
+            //Debug.Log("Clicking in station");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (draggingObj)
+            {
+                draggingObj = false;
+                Debug.Log(draggingObj);
+                chosenBolt.position = originalPosition;
+            }
+        }
+    }
+
+    private void HandleClick(Vector3 position)
+    {
+        Ray ray = cam.ScreenPointToRay(position);
+        if (Physics.Raycast(GetRay(), out RaycastHit rayHit, Mathf.Infinity))
+        {
+            //Debug.Log(rayHit.transform.name);
+            if (rayHit.transform.gameObject.CompareTag("Option"))
+            {
+                Debug.Log("Chose " + rayHit.transform.name);
+                chosenBolt = rayHit.transform;
+                originalPosition = chosenBolt.position;
+                draggingObj = true;
+            }
+        }
     }
 }
--- Assets/Scripts/WorkshopInteraction.cs
+++ Assets/Scripts/WorkshopInteraction.cs
@@ -21,18 +21,22 @@ public class WorkshopInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (workshop_camera.activeSelf)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began) 
+            if (Input.touchCount > 0)
             {
-                HandleClick(Input.GetTouch(0).position);
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    HandleClick(Input.GetTouch(0).position);
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                HandleClick(Input.mousePosition);
             }
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            HandleClick(Input.mousePosition);
-        }
+        
     }
 
     private void HandleClick(Vector3 position) 
@@ -40,15 +44,17 @@ public class WorkshopInteraction : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(position);
         if(Physics.Raycast(ray.origin, ray.direction, out RaycastHit rayHit))
         {
-            if(rayHit.transform.tag == "Station")
+            if(rayHit.transform.gameObject.CompareTag("Station"))
             {
                 clickedObject = rayHit.transform;
                 Debug.Log("Interacting with " + clickedObject.name);
                 current_cam = clickedObject.Find("Camera").gameObject;
 
+
                 current_cam.SetActive(true);
                 inStationUI.SetActive(true);
                 workshop_camera.SetActive(false);
+                ActivateScript();
             }
             else if(clickedObject != null)
             {
@@ -62,7 +68,35 @@ public class WorkshopInteraction : MonoBehaviour
         inStationUI.SetActive(false);
         current_cam.SetActive(false);
         workshop_camera.SetActive(true);
+        DeactivateScript();
 
         clickedObject = null;
+    }
+
+    private void ActivateScript()
+    {
+        if(clickedObject.name == "Tool")
+        {
+            clickedObject.GetComponent<ToolStation>().enabled = true;
+            clickedObject.GetComponent<BoxCollider>().enabled = false;
+        }
+        else if (clickedObject.name == "Paint")
+        {
+            clickedObject.GetComponent<PaintStation>().enabled = true;
+        }
+
+    }
+
+    private void DeactivateScript()
+    {
+        if (clickedObject.name == "Tool")
+        {
+            clickedObject.GetComponent<ToolStation>().enabled = false;
+            clickedObject.GetComponent<BoxCollider>().enabled = true;
+        }
+        else if (clickedObject.name == "Paint")
+        {
+            clickedObject.GetComponent<PaintStation>().enabled = false;
+        }
     }
 }
